788da49230e502c270a2df8450254fc803da0b8c
inhibitor <inhibitor@kaist.ac.kr>
2021-02-26 11:45:45 +0900

    fix: dist

--- dist/components/SwipeablePanelPageLabel.js
+++ dist/components/SwipeablePanelPageLabel.js
@@ -6,7 +6,7 @@ import { jsxs as _jsxs, jsx as _jsx } from "react/jsx-runtime";
 import styled from "styled-components";
 var StyledPageLabel = styled.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  position: absolute;\n  bottom: 4px;\n  right: 4px;\n\n  padding: 0 4px;\n  background-color: rgba(0, 0, 0, 0.7);\n  border-radius: 2px;\n\n  span {\n    font-size: 12px;\n    color: #ffffff;\n  }\n"], ["\n  position: absolute;\n  bottom: 4px;\n  right: 4px;\n\n  padding: 0 4px;\n  background-color: rgba(0, 0, 0, 0.7);\n  border-radius: 2px;\n\n  span {\n    font-size: 12px;\n    color: #ffffff;\n  }\n"])));
 export var SwipeablePanelPageLabel = function (_a) {
-    var key = _a.key, length = _a.length, currentPage = _a.currentPage;
-    return (_jsx(StyledPageLabel, { children: _jsxs("span", { children: [currentPage + 1, "/", length] }, void 0) }, key));
+    var length = _a.length, currentPage = _a.currentPage;
+    return (_jsx(StyledPageLabel, { children: _jsxs("span", { children: [currentPage + 1, "/", length] }, void 0) }, void 0));
 };
 var templateObject_1;
