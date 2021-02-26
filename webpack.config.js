348a16d7e2ec8aba5870c4d8fd7a5d06cab42b19
inhibitor <inhibitor@kaist.ac.kr>
2021-02-26 14:21:26 +0900

    initialize example module

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./examples/__build__"),
+    publicPath: "/__build__/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@inhibitor1217/react-swipeablepanel": path.resolve(__dirname, "./src"),
+    },
+  },
+  entry: "./examples/app.js",
+};
